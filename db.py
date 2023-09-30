7b0580d8ffb97aefc6b5d846c2adc3c03d1b2019
root9464 <senpai9464@yandex.ru>
2023-10-01 02:28:43 +0500

    xx.06.2023

--- db.py
+++ db.py
@@ -4,8 +4,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 ROOT = path.dirname(path.realpath(__file__))
-
 SQLALCHEMY_DATABASE_URL = f"sqlite:///{ROOT}/db.db"
+
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
